9092f622652464c9aeef6269635e843c3444ed3c
Steph Janssen <steph.janssen@on2it.net>
2021-12-26 10:06:22 +0100

    GetTriangularNumber works!

--- lms/lms.go
+++ lms/lms.go
@@ -1,13 +1,10 @@
 package lms
 
-import "fmt"
-
 // GetTriangularNumber finds the Nth triangular number.
 // Formule Triangular numbers: https://www.mathsisfun.com/algebra/triangular-numbers.html.
 // Rule: xn = n(n+1)/2
 func GetTriangularNumber(nth int) int {
-	fmt.Println(nth)
-	return 0
+	return nth * (nth + 1) / 2
 }
 
 func FindDivsors() {
--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/CZero/euler/lms"
+)
+
+func main() {
+	fmt.Println(lms.GetTriangularNumber(3))
+	fmt.Println(lms.GetTriangularNumber(5))
 }
