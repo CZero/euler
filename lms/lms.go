4ba81763e58713e6774be4490c806948548a927c
Steph Janssen <steph.janssen@on2it.net>
2021-12-26 09:54:50 +0100

    GetTriangularNumber start

--- lms/lms.go
+++ lms/lms.go
@@ -1,10 +1,13 @@
 package lms
 
+import "fmt"
+
 // GetTriangularNumber finds the Nth triangular number.
 // Formule Triangular numbers: https://www.mathsisfun.com/algebra/triangular-numbers.html.
 // Rule: xn = n(n+1)/2
-func GetTriangularNumber() {
-	panic("Not implemented")
+func GetTriangularNumber(nth int) int {
+	fmt.Println(nth)
+	return 0
 }
 
 func FindDivsors() {
